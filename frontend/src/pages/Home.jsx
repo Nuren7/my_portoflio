957ac126f23d9117bf88a94653128a15f3c617b1
Nuren7 <nurenislam01@gmail.com>
2026-02-19 21:17:23 +0100

    v1 home hero photos added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 function Home() {
   return (
-    <section>Home</section>
+    <section className="h-screen flex items-start">
+      <div
+        className="
+          flex
+          items-center
+          max-w-7xl
+          mx-auto
+          w-full
+        "
+      >
+        <img 
+          className="h-30"
+          src="cybersecurity_button.png" 
+          alt="cybersecurity" 
+        />
+
+        <img 
+          className="h-[480px]"
+          src="img_of_me2.png" 
+          alt="hero_img" 
+        />
+
+        <img 
+          className="pl-6 translate-y-5 h-30" 
+          src="coder_button.png" 
+          alt="coder_button"
+        />
+      </div>
+    </section>
   )
 }
 
