87910a9b9d04465bc732bfae6c14181b3f35b610
Nuren7 <nurenislam01@gmail.com>
2026-02-21 14:48:02 +0100

    v1.5 Home Hero animations have been added.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom' 
+/* import { useLocation } from 'react-router-dom'  */
+
+
 
 import ScrollFadeIn from '../components/ScrollFadeIn'
 function Home() {
@@ -16,23 +18,22 @@ function Home() {
           max-w-7xl
           mx-auto
           w-full
-          animate-fade-in
         "
       >
         <img 
-          className="-translate-y-6 translate-x-7 h-30"
+          className="-translate-y-6 translate-x-7 h-30 animate-slide-In-Left"
           src="cybersecurity_button.png" 
           alt="cybersecurity" 
         />
 
         <img 
-          className="-translate-y-20 -translate-x-7 h-137.5"
+          className="-translate-y-20 -translate-x-7 h-137.5 animate-slide-In-Down"
           src="img_of_me2.png" 
           alt="hero_img" 
         />
 
         <img 
-          className="translate-y-2 -translate-x-11 h-30" 
+          className="translate-y-2 -translate-x-11 h-30 animate-slide-In-Right" 
           src="coder_button.png" 
           alt="coder_button"
         />
