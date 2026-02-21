1f959c66957d02addf2454095d5aa5c974b8531d
Nuren7 <nurenislam01@gmail.com>
2026-02-21 23:52:04 +0100

    v1.7.1 responsiveness added

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -9,20 +9,20 @@ function Hero() {
         <img
           src="cybersecurity_button.png"
           alt="cybersecurity"
-          className="absolute left-0 bottom-[10%] w-[20%] max-w-[150px] z-10 animate-slide-In-Left"
+          className="absolute left-0 top-[45%] w-[22%] max-w-full z-10 animate-slide-In-Left"
         />
 
         {/* Center image */}
         <img
           src="img_of_me2.png"
           alt="hero_img"
-          className="relative w-[50%] max-w-[600px] z-20 animate-slide-In-Down"
+          className="relative w-[66%] max-w-full z-20 animate-slide-In-Down"
         />
 
         <img
           src="coder_button.png"
           alt="coder_button"
-          className="absolute right-0 bottom-[10%] w-[20%] max-w-[150px] z-10 animate-slide-In-Right"
+          className="absolute right-0 top-[40%] w-[20%] max-w-full z-10 animate-slide-In-Right"
         />
 
       </div>
